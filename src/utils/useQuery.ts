9cd76db8c9478098ce9786c1cc664a5cfd907f6c
eklent ismaili <ismailieklent3@gmail.com>
2024-05-27 20:11:36 +0200

    added custom fetch hook

---
+++ src/utils/useQuery.ts
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import { serverBaseUrl } from "../settings";
+
+interface Props {
+  url: string;
+  onSuccess?: (res: any) => void;
+  onError?: () => void;
+  onPrepareResponse?: (res: any) => any;
+  deps?: any[];
+}
+
+export const useQuery = ({
+  url,
+  onSuccess,
+  onError,
+  onPrepareResponse,
+  deps = [],
+}: Props) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchUrl = serverBaseUrl + url;
+
+  const dataFetch = async () => {
+    try {
+      const data = await (await fetch(fetchUrl)).json();
+
+      // set state when the data received
+      if (onPrepareResponse) {
+        setData(onPrepareResponse(data));
+      } else {
+        setData(data);
+      }
+
+      if (onSuccess) onSuccess(data);
+    } catch (e) {
+      if (onError) onError();
+
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    dataFetch();
+  }, [...deps]);
+
+  return {
+    data,
+    loading,
+    dataFetch,
+  };
+};
