20ad80c38f8f9614e761dd267dd6f81a91c3848e
eklent ismaili <ismailieklent3@gmail.com>
2024-05-27 20:13:20 +0200

    Users basic preview

---
+++ src/containers/users/config.ts
@@ -0,0 +1,22 @@
+export const columns = [
+  {
+    key: "id",
+    dataIndex: "id",
+    title: "ID",
+  },
+  {
+    key: "name",
+    dataIndex: "name",
+    title: "Name",
+  },
+  {
+    key: "email",
+    dataIndex: "email",
+    title: "Email",
+  },
+  {
+    key: "phone",
+    dataIndex: "phone",
+    title: "Phone",
+  },
+];
--- src/containers/users/index.tsx
+++ src/containers/users/index.tsx
@@ -1,3 +1,46 @@
+import { Button, Col, Empty, Row, Skeleton, Table, Typography } from "antd";
+import { useQuery } from "../../utils/useQuery";
+import { columns } from "./config";
+// import { useMemo } from "react";
+import { PlusOutlined } from "@ant-design/icons";
+
+const { Title } = Typography;
+
 export default function Users() {
-  return <div>Users</div>;
+  const { data: users, loading } = useQuery({
+    url: "users",
+  });
+
+  if (loading) return <Skeleton active />;
+
+  if (!users) return <Empty />;
+
+  // const usersColumns = useMemo(() => {
+  //   return {
+  //     ...columns,
+  //     actions: {
+  //       key: "actions",
+  //       title: "Actions",
+  //       render: (_, record: any) => {
+  //         return <>hi</>;
+  //       },
+  //     },
+  //   };
+  // }, []);
+
+  return (
+    <Row gutter={[16, 16]} justify="end">
+      <Col xs={24}>
+        <Title level={2}>Users</Title>
+      </Col>
+      <Col>
+        <Button icon={<PlusOutlined />} danger type="primary">
+          Create New User
+        </Button>
+      </Col>
+      <Col xs={24}>
+        <Table columns={columns} dataSource={[...users]} />
+      </Col>
+    </Row>
+  );
 }
